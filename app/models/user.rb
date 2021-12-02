6b82b61f3f97b74da148c49135b5b9fb84f93b28
Han Fiden <fidendu2@gmail.com>
2021-12-03 00:44:20 +0100

    breh...

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :meetings
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
