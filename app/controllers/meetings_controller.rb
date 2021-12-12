0a0ec2ebc04945fc28537e592c5c3afad683f0c3
Han Fiden <fidendu2@gmail.com>
2021-12-12 19:58:39 +0100

    fix bug

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -1,7 +1,6 @@
 class MeetingsController < ApplicationController
   def index
-  # Scope your query to the dates being shown:
     start_date = params.fetch(:start_date, Date.today).to_date
-    @meetings = Meeting.where(starts_at: start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week)
+    @meetings = Meeting.where(start_time: start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week)
   end
 end
