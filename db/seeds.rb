f69b6a1c6f1b192a3660314ff2fb40da1be3c6bc
Han Fiden <fidendu2@gmail.com>
2021-12-12 20:46:44 +0100

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Clearing meetings database"
+Meeting.destroy_all
+
+puts "Generated Meetings"
+Meeting.create(name: 'test1', start_time: DateTime.now(), end_time: '2021-12-24', user_id: 1)
+puts "1 Meeting"
+Meeting.create(name: 'test2', start_time: '2021-12-14', end_time: '2021-12-17', user_id: 1)
+puts "1 Meeting"
+Meeting.create(name: 'test3', start_time: '2021-12-11', end_time: '2021-12-18', user_id: 1)
+puts "1 Meeting"
+Meeting.create(name: 'test4', start_time: '2021-12-07', end_time: '2021-12-11', user_id: 1)
+puts "1 Meeting"
