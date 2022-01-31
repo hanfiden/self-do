fbf93c66918c1bdab8a1d1d6af30faa9d21edb6a
Han Fiden <fidendu2@gmail.com>
2022-01-31 20:07:44 +0100

    Sloppy but working boolean button

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'meetings#index'
 
-  resources :meetings, only: %I[index create update destroy]
+  resources :meetings, only: %I[index create update destroy] do
+    member do
+      patch :toggle_enable_status
+    end
+  end
 end
