d4470892967c608e9e14e31aa05fb5bad70610ee
Han Fiden <fidendu2@gmail.com>
2022-02-01 11:08:00 +0100

    Add 1 more meeting in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,11 +2,13 @@ puts "Clearing meetings database"
 Meeting.destroy_all
 
 puts "Generated Meetings"
-Meeting.create(name: 'test1', start_time: DateTime.now(), end_time: '2021-12-24', user_id: 1)
+Meeting.create!(name: 'test1', start_time: DateTime.now(), end_time: '2021-12-24', user_id: 1)
 puts "1 Meeting"
-Meeting.create(name: 'test2', start_time: '2021-12-14', end_time: '2021-12-17', user_id: 1)
+Meeting.create!(name: 'test2', start_time: '2021-12-14', end_time: '2021-12-17', user_id: 1)
 puts "1 Meeting"
-Meeting.create(name: 'test3', start_time: '2021-12-11', end_time: '2021-12-18', user_id: 1)
+Meeting.create!(name: 'test3', start_time: '2021-12-11', end_time: '2021-12-18', user_id: 1)
 puts "1 Meeting"
-Meeting.create(name: 'test4', start_time: '2021-12-07', end_time: '2021-12-11', user_id: 1)
+Meeting.create!(name: 'test4', start_time: '2021-12-07', end_time: '2021-12-11', user_id: 1)
+puts "1 Meeting"
+Meeting.create!(name: 'test5', start_time: '2022-01-07', end_time: '2022-02-11', user_id: 1)
 puts "1 Meeting"
