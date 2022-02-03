c574825031ac56a1a34b890d3b287ef8699cb82c
Han Fiden <fidendu2@gmail.com>
2022-02-03 15:05:31 +0100

    Add new meeting to routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'meetings#index'
 
-  resources :meetings, only: %I[index create update destroy] do
+  resources :meetings, only: %I[index new create update destroy] do
     member do
       patch :toggle_enable_status
     end
